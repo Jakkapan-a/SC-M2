438989fb485693a36b9881e1d6a8285c90825307
Jakkapan <Jakkapan.attala@gmail.com>
2022-12-19 23:05:42 +0700

    Add comment

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -16,18 +16,14 @@ PINOUT alarm(2,true);
 bool stringComplete = false; // whether the string is complete
 String inputString = "";
 bool state_1 = false;
-bool stste_alarm = false;
-
-unsigned long lastDebounceTimeAlarm = 0;
-unsigned long debounceDelayAlarm = 1000;
 
 void serialEvent() {
     while (Serial.available()) {
         // get the new byte:
         char inChar = (char)Serial.read();
         if (inChar == '#') {
-            stringComplete = true; // Set state complete to true
-            inputString.trim();
+            stringComplete = true;  // Set state complete to true
+			inputString.trim();     // Remove space
             break;
         }
         if (inChar == '>' || inChar == '<' || inChar == '\n' || inChar == '\r' || inChar == '\t' || inChar == ' ' || inChar == '?') {
@@ -39,7 +35,6 @@ void serialEvent() {
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(115200);
-    stste_alarm = false;
 
     mes.off();
     mesLED.off();
@@ -48,7 +43,8 @@ void setup() {
 }
 
 void loop() {
-    if (button_1.isPressed() && state_1) {
+	// check the status of the button press.
+	if (button_1.isPressed() && state_1) { // RST
         mes.off();
         mesLED.off();
         imagePrepare.on();
@@ -56,7 +52,8 @@ void loop() {
         state_1 = false;
         Serial.println("RST - PIN");
     }
-    if (stringComplete) {
+	// check to see if the string is complete:
+	if (stringComplete) { // If state complete is true
         if (inputString == "OK") {
             mes.on();
             mesLED.on();
---
+++ README.md
@@ -0,0 +1,27 @@
+# Program checking compare data
+
+This is a simple compare image
+
+## How to use
+1. Database: SQLite
+2. IDE: Visual Studio 2022
+3. Arduino 
+
+## How to run
+1. Open the solution in Visual Studio
+2. Build the solution
+3. Run the solution
+
+<!-- Photo -->
+## Image
+<br>
+<br>
+<p align="center">
+    <img src="./Images/img-1.jpg" alt="" width="500">
+</p>
+
+## How to setting after run program
+1. Click right to settting
+2. Add model and close page
+3. Select model and click edit to update image master
+5. Close page setting and run test program
